8137f812e0a78b0d2e9620a6ee7f0eb0cd93ac4d
divy-sys <divy.varia.1020@gmail.com>
2023-02-17 17:39:04 +0530

    Added helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,7 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "image")
   end
 
-  def current_user 
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
+  
   def logged_in?
   !!current_user 
   end
